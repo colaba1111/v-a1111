4d88bbe051f682c2d84a96d1a2fca90f3f0f6e64
colaba1111 <105175670+colaba1111@users.noreply.github.com>
2024-03-13 20:37:20 +0000

    use huggingface-cli

--- setup.sh
+++ setup.sh
@@ -3,10 +3,13 @@ STABLE_DIFFUSION_DIR=~/stable-diffusion-webui
 EXTENSION_DIR=$STABLE_DIFFUSION_DIR/extensions
 CONTROLNET_MODELS_DIR=$STABLE_DIFFUSION_DIR/extensions/sd-webui-controlnet/models
 DIFFUSION_MODELS_DIR=$STABLE_DIFFUSION_DIR/models/Stable-diffusion
+LORA_MODELS_DIR = $STABLE_DIFFUSION_DIR/models/Lora
+EMBEDDING_MODELS_DIR = $STABLE_DIFFUSION_DIR/models/Lora
 
 # Dependencies
 sudo apt install build-essential aria2 -y
 sudo apt install wget git python3 python3-venv libgl1 libglib2.0-0 -y
+pip install -U "huggingface_hub[cli]"
 
 # A1111
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui $STABLE_DIFFUSION_DIR
@@ -36,43 +39,40 @@ git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git $EX
 # Install adetailer
 git clone https://github.com/Bing-su/adetailer.git $EXTENSION_DIR/adetailer
 
-# Download controlnet
-# Newer models for faceid
-# aria2c https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter-full-face_sd15.safetensors -d $CONTROLNET_MODELS_DIR -o ip-adapter-full-face_sd15.safetensors
-# aria2c https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter-plus-face_sd15.safetensors -d $CONTROLNET_MODELS_DIR -o ip-adapter-plus-face_sd15.safetensors
-# aria2c https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter-plus_sd15.safetensors -d $CONTROLNET_MODELS_DIR -o ip-adapter-plus_sd15.safetensors
-# aria2c https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter_sd15.safetensors -d $CONTROLNET_MODELS_DIR -o ip-adapter_sd15.safetensors
+# Install 
+git clone https://github.com/ahgsql/StyleSelectorXL $EXTENSION_DIR/StyleSelectorXL
 
-# https://huggingface.co/h94/IP-Adapter-FaceID
-aria2c https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plus_sd15.bin?download=true -d $CONTROLNET_MODELS_DIR -o ip-adapter-faceid-plus_sd15.bin
-# ip-adapter-faceid-plus_sd15_lora.safetensors
-aria2c https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sd15.bin?download=true -d $CONTROLNET_MODELS_DIR -o ip-adapter-faceid-plusv2_sd15.bin
-# ip-adapter-faceid-plusv2_sd15_lora.safetensors
-aria2c https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sdxl.bin?download=true -d $CONTROLNET_MODELS_DIR -o ip-adapter-faceid-plusv2_sdxl.bin
-# ip-adapter-faceid-plusv2_sdxl_lora.safetensors
-aria2c https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid_sd15.bin?download=true -d $CONTROLNET_MODELS_DIR -o ip-adapter-faceid_sd15.bin
-aria2c https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid_sdxl.bin?download=true -d $CONTROLNET_MODELS_DIR -o ip-adapter-faceid_sdxl.bin
-aria2c https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-portrait_sd15.bin?download=true -d $CONTROLNET_MODELS_DIR -o ip-adapter-faceid-portrait_sd15.bin
+# InstantX/InstantID
+# TODO
 
-# https://huggingface.co/InstantX/InstantID
-aria2c https://huggingface.co/InstantX/InstantID/resolve/main/ip-adapter.bin?download=true -d $CONTROLNET_MODELS_DIR -o ip-adapter_instant_id_sdxl.bin
-aria2c https://huggingface.co/InstantX/InstantID/resolve/main/ControlNetModel/diffusion_pytorch_model.safetensors?download=true -d $CONTROLNET_MODELS_DIR -o control_instant_id_sdxl.safetensors
+# IP-Adapter
+huggingface-cli download h94/IP-Adapter models/ip-adapter-plus-face_sd15.safetensors --local-dir $CONTROLNET_MODELS_DIR
+huggingface-cli download h94/IP-Adapter models/ip-adapter-plus_sd15.safetensors --local-dir $CONTROLNET_MODELS_DIR
+mv $CONTROLNET_MODELS_DIR/models $CONTROLNET_MODELS_DIR
+rmdir $CONTROLNET_MODELS_DIR/models
 
-#  https://huggingface.co/CrucibleAI/ControlNetMediaPipeFace
-aria2c https://huggingface.co/CrucibleAI/ControlNetMediaPipeFace/resolve/main/control_v2p_sd15_mediapipe_face.safetensors?download=true -d $CONTROLNET_MODELS_DIR -o control_v2p_sd15_mediapipe_face.safetensors
+# IP-Adapter SDXL
+huggingface-cli download h94/IP-Adapter sdxl_models/ip-adapter-plus-face_sdxl_vit-h.safetensors --local-dir $CONTROLNET_MODELS_DIR
+huggingface-cli download h94/IP-Adapter sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors --local-dir $CONTROLNET_MODELS_DIR
+mv $CONTROLNET_MODELS_DIR/sdxl_models $CONTROLNET_MODELS_DIR
+rmdir $CONTROLNET_MODELS_DIR/sdxl_models
 
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15_canny_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15_lineart_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_mlsd_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15_mlsd_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15_openpose_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_scribble_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15_scribble_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_seg_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15_seg_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_softedge_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15_softedge_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15s2_lineart_anime_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15s2_lineart_anime_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_softedge.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11p_sd15_softedge.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/t2iadapter_color_sd14v1.pth -d $CONTROLNET_MODELS_DIR -o t2iadapter_color_sd14v1.pth
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors -d $CONTROLNET_MODELS_DIR -o control_v11f1e_sd15_tile_fp16.safetensors
-aria2c https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/t2iadapter_color_sd14v1.pth -d $CONTROLNET_MODELS_DIR -o t2iadapter_color_sd14v1.pth
+# IP-Adapter-FaceID
+huggingface-cli download h94/IP-Adapter-FaceID ip-adapter-faceid-plusv2_sd15.bin --local-dir $CONTROLNET_MODELS_DIR
+huggingface-cli download h94/IP-Adapter-FaceID ip-adapter-faceid-plusv2_sd15_lora.safetensors --local-dir $LORA_MODELS_DIR
+
+# IP-Adapter-FaceID SDXL
+huggingface-cli download h94/IP-Adapter-FaceID ip-adapter-faceid-plusv2_sdxl.bin --local-dir $CONTROLNET_MODELS_DIR
+huggingface-cli download h94/IP-Adapter-FaceID ip-adapter-faceid-plusv2_sdxl_lora.safetensors --local-dir $LORA_MODELS_DIR
+
+# Controlnet-v1-1
+huggingface-cli download lllyasviel/ControlNet-v1-1 --include *.pth *.yaml --local-dir $CONTROLNET_MODELS_DIR
+
+# MediaPipeFace
+huggingface-cli download CrucibleAI/ControlNetMediaPipeFace control_v2p_sd15_mediapipe_face.safetensors control_v2p_sd15_mediapipe_face.yaml --local-dir $CONTROLNET_MODELS_DIR
 
 # Model
-aria2c https://huggingface.co/SG161222/Realistic_Vision_V5.1_noVAE/resolve/main/Realistic_Vision_V5.1.safetensors?download=true -d $DIFFUSION_MODELS_DIR -o realisticVisionV51_v51VAE.safetensors
+aria2c https://civitai.com/api/download/models/130072?type=Model&format=SafeTensor&size=pruned&fp=fp16 -d $DIFFUSION_MODELS_DIR -o realisticVisionV51_v51VAE.safetensors
+
+# Texual Inversion
+# TODO
